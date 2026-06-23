4b66319e1cb63ea3c6836caa352dabd2b17e9328
zyxisme <d0116u@gmail.com>
2026-06-23 09:34:04 +0800

    fix: revert to 100dvh, remove css-has-pseudo polyfill for Chrome 99 compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 import postcss_color_mix from '@csstools/postcss-color-mix-function'
 import postcss_oklab from '@csstools/postcss-oklab-function'
 import tailwindcss from '@tailwindcss/postcss'
-import css_has_pseudo from 'css-has-pseudo'
 import postcss_conditionals from 'postcss-conditionals'
 import postcss_for from 'postcss-for'
 import postcssOklchStaticFallback from './postcss-oklch-static-fallback.mjs'
@@ -14,6 +13,5 @@ export default {
     postcssOklchStaticFallback(),
     postcss_oklab({ preserve: false }),
     postcss_color_mix({ preserve: false }),
-    css_has_pseudo({ preserve: false }),
   ],
 }
--- src/App.vue
+++ src/App.vue
@@ -206,7 +206,7 @@ useKeyboard()
         `custom-background-${dashboardTransparent} custom-background bg-cover bg-center`,
       blurClass,
     ]"
-    :style="[backgroundImage, { height: 'var(--app-height, 100vh)' }]"
+    :style="[backgroundImage, { height: 'var(--app-height, 100dvh)' }]"
   >
     <RouterView />
     <div
